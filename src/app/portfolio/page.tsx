5eb60011a31f08ec44d6f1b6c2d54097edf7fdaa
ひらぬま <shuuuya0616@gmail.com>
2024-06-06 17:26:48 +0900

    ポートフォリオページをいい感じに

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,4 +1,17 @@
+import Image from 'next/image'
 import { Link, Text, Title } from '~/app/_component/ui'
+
+const data = [
+  {
+    title: 'エンジニアDB',
+    description: 'エンジニア同士が交流するためのサービスです。',
+    url: 'https://www.engineer-db.com',
+    image: {
+      src: '/portfolio/engineer_db.jpg',
+      alt: 'エンジニアDBのサイトイメージ',
+    },
+  },
+]
 
 export default function Home() {
   return (
@@ -7,12 +20,25 @@ export default function Home() {
         <Title as="h1" className="">
           Portfolio
         </Title>
-        <Text>私が作ったものを紹介します。</Text>
+        <Text>これまで作ったものを紹介します。</Text>
       </div>
-      <div className="grid gap-6">
-        <Link href="https://www.engineer-db.com" target="_blank">
-          エンジニアDB
-        </Link>
+      <div className="grid gap-8 md:grid-cols-2">
+        {data.map((item) => (
+          <Link
+            key={item.title}
+            href={item.url}
+            target="_blank"
+            className="grid gap-2 no-underline group hover:-translate-y-[2px] transition-all duration-300"
+          >
+            <Image src={item.image.src} alt={item.image.alt} width={800} height={420} objectFit="contain" />
+            <div className="group-hover:underline">
+              <Title as="h2" size="md">
+                {item.title}
+              </Title>
+              <Text>{item.description}</Text>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   )
